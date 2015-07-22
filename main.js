2a1f06edeb36f7db8808a1cc22d8272bd000c984
Jesse Turner <jesseturner@comcast.net>
2015-07-22 09:49:50 -0500

    multi-divi final version

---
+++ main.js
@@ -0,0 +1,40 @@
+// - In HTML, have a single text input field
+// - In HTML , have a button
+// - In JavaScript, have a function divides a number by 10
+// - In JavaScript, have a function that multiplies a number by 5
+// - When the button is clicked, call a JavaScript function that checks if the user has entered
+//  in a value in the input field
+// - If there is no value, put an appropriate message in an alert box telling the user to enter 
+// something
+// - If there is a value, and its numeric value is greater than 10000, call a function that 
+// passes in the user-entered value as an argument, and the name of the function that divides 
+// by 10.
+// -If there is a value, and its numeric value is less than 10000, call a function that passes
+//  in the user-entered value as an argument, and the name of the function that multiplies by 5. 
+//  Write the result of the calculation into the HTML document.
+
+function divi(diviarg) {
+	return diviarg / 10;
+}
+
+function multi(multiarg) {
+	return multiarg * 5;
+}
+
+function whenClicked() {
+	var userValue = document.getElementById("num1").value;
+	if (userValue === "") {
+		alert("Gimme a number, I said!")
+	} else {
+		if (userValue > 10000) {
+			document.getElementById("result").innerHTML = "<p>Here's your new number: " + 
+			divi(userValue) + "</p><p>You're welcome.</p>";
+		} else {
+			document.getElementById("result").innerHTML =  "<p>Here's your new number: " + 
+			multi(userValue) + "</p><p>You're welcome.</p>";
+		}
+	}
+}
+
+
+
